7385c9a62b7da71630c4255d0d6ca55cbf2aee98
FredP21 <f.pelletier21@gmail.com>
2023-10-12 17:14:45 +0200

    I use props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,4 @@
-const PokemonCard = () => {
-    const pokemonList = [
-        {
-          name: "bulbasaur",
-          imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-        },
-        {
-          name: "mew",
-        },
-      ];
-      const pokemon = pokemonList[0]
-      
+const PokemonCard = ({pokemon}) => {
 
     return ( 
         <figure>
